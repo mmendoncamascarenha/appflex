fa9238fe732e207b6387391202223c68fb64af0b
Murillo Mendonca Mascarenha <mmendoncamascarenha@gmail.com>
2025-01-22 15:16:35 -0300

    app flex

---
+++ app.js
@@ -0,0 +1,22 @@
+/**
+ * APP - Flex
+ * @author Murillo Mendonça Mascarenha 
+ */
+//declaração de variáveis
+let etanol, gasolina
+
+function calcular() {
+    //entrada de dados(input)
+    etanol = frmFlex.inputEtanol.value
+    gasolina = frmFlex.inputGasolina.value
+    //console.log(etanol)
+    //console.log(gasolina)
+    if (etanol < 0.7 * gasolina) {
+        document.getElementById('status').src="img/etanol.png"
+    } else {
+        document.getElementById('status').src="img/gasolina.png"
+
+    }
+    return false 
+
+}
